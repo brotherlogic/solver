b6a6e75c3f3bb78cdca6203c5a63977ac897e61d
Simon Tucker <simon@brotherlogic@gmail>
2019-04-07 04:21:13 -0700

    Adds problem2

--- solverapi.go
+++ solverapi.go
@@ -18,8 +18,12 @@ func (s *Server) Solve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResp
 
 func (s *Server) solveProblem(req *pb.SolveRequest) (*pb.SolveResponse, error) {
 	solution := int64(0)
-	for i := req.KeyStart; i < req.KeyEnd; i += req.Step {
-		solution += s.solve(req.Problem, i)
+	if req.Problem == 1 {
+		for i := req.KeyStart; i < req.KeyEnd; i += req.Step {
+			solution += s.solve(req.Problem, i)
+		}
+	} else if req.Problem == 2 {
+		solution = s.solve(req.Problem, req.KeyEnd)
 	}
 	return &pb.SolveResponse{Solution: solution}, nil
 }
@@ -40,7 +44,11 @@ func (s *Server) distributeProblem(ctx context.Context, req *pb.SolveRequest) (*
 		if err != nil {
 			return nil, fmt.Errorf("Failed to reach friend %v", s.friends[i].Identifier)
 		}
-		solution += resp.Solution
+		if req.Problem == 2 {
+			solution = resp.Solution
+		} else {
+			solution += resp.Solution
+		}
 	}
 	return &pb.SolveResponse{Solution: solution, Nodes: int32(len(s.friends))}, nil
 }
