0d191f2fc193d8779ef9cbdb5d240f933f111165
Simon Tucker <simon@brotherlogic@gmail>
2019-04-07 08:16:16 -0700

    Fixer

--- solverutils.go
+++ solverutils.go
@@ -18,13 +18,18 @@ func (s *Server) solve2(i int64) int64 {
 	s2 := int64(2)
 	t := int64(0)
 	sum := int64(2)
+	found := int64(0)
 	for s2 < i {
 		t = s2 + s1
 		s1 = s2
 		s2 = t
 		if s2%2 == 0 {
+			found = int64(1)
 			sum += s2
+		} else {
+			found = int64(0)
 		}
 	}
-	return sum - s2
+
+	return sum - (s2 * found)
 }
