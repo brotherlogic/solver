f2f9b22ba75dc4085897169ffc5cf37f8753315d
Simon Tucker <simon@brotherlogic@gmail>
2019-04-07 04:33:42 -0700

    Coverage

--- solverapi_test.go
+++ solverapi_test.go
@@ -40,6 +40,22 @@ func TestProblem(t *testing.T) {
 	}
 }
 
+func TestProblem2(t *testing.T) {
+	s := Init()
+	s.test = true
+	s.pass = true
+	s.friends = append(s.friends, &pbd.RegistryEntry{})
+	r, err := s.Solve(context.Background(), &pb.SolveRequest{Step: 10, Problem: 2, KeyStart: 1, KeyEnd: 200})
+
+	if err != nil {
+		t.Errorf("Oops: %v", err)
+	}
+
+	if r.Solution == 0 {
+		t.Errorf("Problem in solution: %v", r)
+	}
+}
+
 func TestNoFriends(t *testing.T) {
 	s := Init()
 	s.test = true
